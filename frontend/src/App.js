28c5316d9ddc8673f7a576109cd3d9b91656b04c
Brise <gabriel_ns@live.com>
2021-11-21 23:54:55 -0300

    fix: static file serving

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,13 @@ class App extends Component {
                 href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
                 rel="stylesheet"
               ></link>
-              <link href="/index.css" rel="stylesheet" type="text/css" />
+              <link
+                rel="stylesheet"
+                href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+                integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+                crossorigin="anonymous"
+              />
+              <link href="index.css" rel="stylesheet" />
             </Helmet>
             <BitcoinSection />
             <MarketSection />
